33d131cb04558d5a823f5bb0c47a73d9d919bcf7
hemalathareddy20 <hemalathareddy401@gmail.com>
2020-11-26 16:00:51 +0530

    Added:Logic to check First name with Caps and hasminimum 3 characters

--- registration.sh
+++ registration.sh
@@ -1,1 +1,9 @@
 echo "Welcome of the User Registration program being solved in the Master Branch"
+read -p "enter your first name: " firstname
+firstnameRegex="^[A-Z]{1}[a-z]{2,}"
+if(($firstname =~ $firstnameRegex))
+then
+      echo "$firstname is a valid name"
+else
+     echo "invalid name"
+fi
